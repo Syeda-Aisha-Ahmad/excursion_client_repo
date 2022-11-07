e2473224aeee0c669506e9460618da803f7d14c2
Syeda-Aisha-Ahmad <18aishaahmad@gmail.com>
2022-11-08 02:33:54 +0600

    Dynamic title added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import useTitle from '../../Hooks/useTitle';
 
 const Home = () => {
+    //Title
+    useTitle('Home')
     return (
         <div>
             This is home page
