7957634fe299cca808192f146bc4dda61b1d1980
Syeda-Aisha-Ahmad <18aishaahmad@gmail.com>
2022-11-08 02:25:10 +0600

    Favicon added

--- src/Routes/Main/Main.js
+++ src/Routes/Main/Main.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../../Shared/Footer/Footer';
+import Nav from '../../Shared/Nav/Nav';
 
 const Main = () => {
     return (
         <div>
+            <Nav></Nav>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
